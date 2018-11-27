775e28b4d1e640dc101f9ec3cbb279e9c821afc5
Sainishma <45390371+Sainishma@users.noreply.github.com>
2018-11-27 19:40:20 +0530

    done with first one

---
+++ Firstjavafile.java
@@ -0,0 +1,11 @@
+import java.lang.io.*;
+Class Newclass
+{
+public vooid method1()
+{
+System.out.println("hello world");
+}
+public ststic void main(String args[])
+Newclass n=new Newclass();
+n.method1();
+}
